ec99d7ba100b40beb79da4796fdf82a1cd2f7168
Eisenwave <me@eisenwave.net>
2021-11-11 16:02:48 +0100

    implement suboptimal use and non-canonical commutativity optimizations for 2x, 0.9x speedup respectively

--- bruteforce.cpp
+++ bruteforce.cpp
@@ -6,7 +6,7 @@ namespace {
 
 [[nodiscard]] std::uint8_t distance_from_inputs(const CanonicalProgram &program, const unsigned operand) noexcept
 {
-    return operand < 6 ? 0 : program[operand - 6].distance;
+    return operand < VARIABLE_COUNT ? 0 : program[operand - VARIABLE_COUNT].distance;
 }
 
 [[nodiscard]] bool is_breaking_canonical_dag_order(const CanonicalProgram &program,
@@ -23,7 +23,8 @@ namespace {
 
 [[nodiscard]] bool is_double_negation(const CanonicalProgram &program, const CanonicalInstruction ins) noexcept
 {
-    return static_cast<Op>(ins.op) == Op::NOT_A && ins.a >= 6 && static_cast<Op>(program[ins.a - 6].op) == Op::NOT_A;
+    return static_cast<Op>(ins.op) == Op::NOT_A && ins.a >= VARIABLE_COUNT &&
+           static_cast<Op>(program[ins.a - VARIABLE_COUNT].op) == Op::NOT_A;
 }
 
 [[nodiscard]] bool contains(const CanonicalProgram &program, const CanonicalInstruction ins) noexcept
@@ -38,12 +39,62 @@ namespace {
 
 [[nodiscard]] bool are_complement_of_same_input(const CanonicalProgram &program, unsigned a, unsigned b) noexcept
 {
+    // FIXME given operand generation, can b ever be lower than a ? (probably not)
     swap_if(a, b, b < a);
-    if (b < 6) {
+    if (b < VARIABLE_COUNT) {
         return false;
     }
     const CanonicalInstruction ins = program[b - 6];
     return static_cast<Op>(ins.op) == Op::NOT_A && program[b - 6].a == a;
+}
+
+[[nodiscard]] bool is_using_operand(const CanonicalProgram &program,
+                                    const std::uint8_t operand,
+                                    const CanonicalInstruction ins) noexcept
+{
+    const auto check_use = [&program, operand](std::uint8_t other) {
+        return other == operand ||
+               (other >= VARIABLE_COUNT && is_using_operand(program, operand, program[other - VARIABLE_COUNT]));
+    };
+
+    return check_use(ins.a) || (not op_is_unary(static_cast<Op>(ins.op)) && check_use(ins.b));
+}
+
+[[nodiscard]] bool is_suboptimal_and_or(const CanonicalProgram &program, const CanonicalInstruction ins) noexcept
+{
+    const auto op = static_cast<Op>(ins.op);
+    return (op == Op::AND || op == Op::OR) && ins.b >= VARIABLE_COUNT &&
+           is_using_operand(program, ins.a, program[ins.b - VARIABLE_COUNT]);
+}
+
+[[nodiscard]] bool is_non_canonical_commutative(const CanonicalProgram &program,
+                                                const CanonicalInstruction ins) noexcept
+{
+    const auto op = static_cast<Op>(ins.op);
+    if (not op_is_commutative(op) || ins.b < VARIABLE_COUNT) {
+        return false;
+    }
+    const CanonicalInstruction other = program[ins.b - VARIABLE_COUNT];
+    if (ins.op != other.op) {
+        return false;
+    }
+
+    const std::uint8_t a = ins.a;
+    const std::uint8_t b = other.a;
+    const std::uint8_t c = other.b;
+
+    // already in canonical order, nothing left to check
+    if (a < b) {
+        return false;
+    }
+
+    // if the instructions aren't equidistant,
+    // then requiring canonical ordering could also make valid programs impossible (possibly ?)
+    const std::uint8_t dist_a = distance_from_inputs(program, a);
+    const std::uint8_t dist_b = distance_from_inputs(program, b);
+    const std::uint8_t dist_c = distance_from_inputs(program, c);
+
+    return dist_a == dist_b && dist_a == dist_c;
 }
 
 template <bool Unary>
@@ -82,15 +133,26 @@ template <bool Unary>
         return false;
     }
 
-    // 4 prevent duplicate evaluations
-    if (contains(program, ins)) {
+    // 4 non-canonical ordering of commutative operations, e.g. C and (A and B)
+    //   only A and (B and C) is allowed
+    if (not Unary && is_non_canonical_commutative(program, ins)) {
         return false;
     }
 
-    // 5 prevent creation of zombie programs
+    // 5 suboptimal use of and/or, e.g. A and SubExpr where A appears in SubExpr
+    if (not Unary && is_suboptimal_and_or(program, ins)) {
+        return false;
+    }
+
+    // 6 prevent creation of zombie programs
     //   i.e. programs with so many dead (unused) instructions, that even after the addition of the given instruction,
     //   not all subexpressions of the program can be used
     if (is_program_unrevivable<Unary>(program, ins.a, ins.b)) {
+        return false;
+    }
+
+    // 7 prevent duplicate evaluations
+    if (contains(program, ins)) {
         return false;
     }
 
